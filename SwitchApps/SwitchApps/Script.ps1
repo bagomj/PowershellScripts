f167d6d26631ed7a2cf26a9503386c7945160d97
BagomJ <BagomJ@LHVNSWZ1>
2016-09-26 14:40:31 +0100

    12

--- SwitchApps/SwitchApps/Script.ps1
+++ SwitchApps/SwitchApps/Script.ps1
@@ -3,17 +3,27 @@
 # Version: 1.00
 
 $wshell = New-Object -ComObject wscript.shell;
-$x = 0;# Assigns 
-$b = 1;
 #If 1 is less than 2, then activate Internet explorer
 #In this case, if Firefox is not found, the loop will stop once X reaches 3. 
-While ($x -le $b) {
-$wshell.AppActivate('Internet Explorer')
-Sleep 4
-$wshell.AppActivate('Firefox')
-Sleep 4
-if((get-process "firefox" -ea SilentlyContinue) -eq $Null){ 
+$x = 0;
+$b = 1;
+$program1 = 'Firefox'
+$program2 = 'Internet Explorer'
+
+	While ($x -le $b) {
+#AppActivate picks on an application to start
+	$wshell.AppActivate($program2)
+#Each applictaion will sleep for a given amount of time
+	Sleep 4
+	$wshell.AppActivate($program1)
+	Sleep 4
+#If a process cannot be found, it will skip to the next process 
+	if((get-process $program2 -ea SilentlyContinue) -Or (get-process $program1 -ea SilentlyContinue) -eq $Null){ 
        echo "Not Running" 
        $x++ 
 }
+	#if((get-process $program1 -ea SilentlyContinue) -eq $Null){ 
+     #  echo "Not Running" 
+      # $x++ 
+#}
 }
